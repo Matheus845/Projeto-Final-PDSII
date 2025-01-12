91cb091fbea46f5eb9669bbb8493f4efc9ac27c8
Matheus845 <matheus.guedes.belloni1973@gmail.com>
2025-01-12 11:19:45 -0300

    Update jogador.hpp

--- src/players/jogador.hpp
+++ src/players/jogador.hpp
@@ -14,18 +14,28 @@ public:
     std::string getNome() const;
     std::string getEmail() const;
     std::string getSenha() const;
+    int getVitorias() const;
+    int getDerrotas() const;
 
     // Setters
     void setApelido(const std::string& apelido);
     void setNome(const std::string& nome);
     void setEmail(const std::string& email);
     void setSenha(const std::string& senha);
+    void setVitorias(int vitorias);
+    void setDerrotas(int derrotas);
+
+    // Métodos para atualizar estatísticas
+    void incrementarVitorias();
+    void incrementarDerrotas();
 
 private:
     std::string apelido;
     std::string nome;
     std::string email;
     std::string senha;
+    int vitorias;
+    int derrotas;
 };
 
 #endif 
